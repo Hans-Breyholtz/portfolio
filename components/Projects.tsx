a322d54eb584cbfe5bf357e7bdb7391122fe240e
Hans-Eirik <hbreyholtz@gmail.com>
2022-10-13 12:15:23 +0200

    Portfolio

---
+++ components/Projects.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+function Projects({}: Props) {
+  return (
+    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
+
+        <div className='relative w-full flex overflow-x-scroll snap-x snap-mandatory z-20'>
+            
+        </div>
+
+        <div className='w-full absolute top-[30%] bg-[#3CA6A6]/10 left-0 h-[500px] -skew-y-12'></div>
+    </div>
+  )
+}
+
+export default Projects
